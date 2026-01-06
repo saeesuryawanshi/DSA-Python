7feea368713b9516b24b2f77e171b6c93a4863a7
Saee Suryawanshi <saeesuryawanshi1@gmail.com>
2026-01-07 00:00:14 +0530

    Added linked list cycle detection problem

---
+++ linked_list/detect_cycle.py
@@ -0,0 +1,62 @@
+# Detect Cycle in a Singly Linked List using Floyd's Tortoise and Hare Algorithm
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    # Add node at the end
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        last = self.head
+        while last.next:
+            last = last.next
+        last.next = new_node
+
+    # Print the linked list
+    def print_list(self, limit=20):
+        # limit added to prevent infinite loop in case of cycle
+        curr = self.head
+        count = 0
+        while curr and count < limit:
+            print(curr.data, end=" -> ")
+            curr = curr.next
+            count += 1
+        if curr:
+            print("...")
+        else:
+            print("None")
+
+    # Detect cycle
+    def has_cycle(self):
+        slow = self.head
+        fast = self.head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
+
+# Test the linked list cycle detection
+if __name__ == "__main__":
+    ll = LinkedList()
+    ll.append(10)
+    ll.append(20)
+    ll.append(30)
+    ll.append(40)
+
+    # Creating a cycle manually: last node points to second node
+    ll.head.next.next.next.next = ll.head.next  
+
+    if ll.has_cycle():
+        print("Cycle detected in the linked list")
+    else:
+        print("No cycle detected")
