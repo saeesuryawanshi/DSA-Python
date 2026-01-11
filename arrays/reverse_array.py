6dab7372807ee8539dc3bf54e3ffa291c0de3c24
Saee Suryawanshi <saeesuryawanshi1@gmail.com>
2026-01-11 17:23:33 +0530

    added reverse array without built-in function and complexity analysis

--- arrays/reverse_array.py
+++ arrays/reverse_array.py
@@ -1,2 +1,21 @@
+#Reverse the array
 arr = [1, 2, 3, 4, 5]
+
+print("usinf built in funtion:")
 print(arr[::-1])
+
+def reverse_arr(arr):
+    i=len(arr)-1
+    while i>=0:
+        print(arr[i],end=" ")
+        i-=1
+      
+print("without using function")
+reverse_arr(arr)
+
+# Time Complexity:
+# Both approaches -> O(n)
+
+# Space Complexity:
+# Built-in slicing -> O(n)
+# Without built-in -> O(1)
